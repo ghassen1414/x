40beaaf618a8c49f3c91db28b6dc4e51696f59b1
Zarrouk <ghassen.zarrouk42@gmail.com>
2024-12-02 17:45:50 +0100

    Initial commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿//Guess the word Game
+
+using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static Random rnd = new Random();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("      Welcome to Number Sequence Memory");
+            int level = 1;
+            string generatedNumber = "";
+            while (true)
+            {
+                Console.WriteLine("Generating Number..");
+                generatedNumber = Add(generatedNumber);
+                Console.WriteLine(generatedNumber);
+                Console.WriteLine("You have 3 seconds to memorize the number");
+                Thread.Sleep(3000);
+                Console.Clear();
+                Console.WriteLine("Enter the Number:");
+                string guessendNumber = Console.ReadLine();
+                if (guessendNumber == generatedNumber)
+                {
+                    Console.WriteLine("Good Job");
+                    level++;
+                }
+                else
+                {
+                    Console.WriteLine("You Lost");
+                    Console.WriteLine("The correct number was"+generatedNumber);
+                    break;
+                }
+
+
+
+
+                string Add(string s)
+                {
+                    return s+ Convert.ToString(rnd.Next(0,10));
+                }
+            }
+        }
+    }
+}
